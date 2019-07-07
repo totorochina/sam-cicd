df5aef4043e20ea76e911eb26c835da91bedaa10
Dave Chen <totorochina@hotmail.com>
2019-07-07 10:10:34 +0800

    to v1.1

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world! v1.0",
+            "message": "hello world! v1.1",
             "location": ip.text.replace("\n", "")
         }),
     }
